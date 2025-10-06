7249cdfb5b7a284504bc16d7c021e2e6cd489e1b
Stefan Helldobler <s.helldobler@gmx.de>
2025-10-06 16:22:54 +0200

    fix: htaccess and dist folder to host

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,11 @@
-
 export const environment = {
   production: true,
   firebase: {
     apiKey: "AIzaSyDylscGM9_VNRa0ItH1uOwFJNyeGsHXT6A",
-    authDomain: "simple-crm-xxxxx.firebaseapp.com",
-    projectId: "simple-crm-xxxxx",
-    storageBucket: "simple-crm-xxxxx.appspot.com",
-    messagingSenderId: "123456789012",
-    appId: "1:123456789012:web:abcdef123456789"
+    authDomain: "simple-crm-c4177.firebaseapp.com",
+    projectId: "simple-crm-c4177",
+    storageBucket: "simple-crm-c4177.firebasestorage.app",
+    messagingSenderId: "1039115084271",
+    appId: "1:1039115084271:web:65082de98d9ea6a939161a"
   }
 };
