9b7bd47eb071effc3bc6b974d5e9f814e3fc93dc
Stefan Helldobler <s.helldobler@gmx.de>
2025-10-06 15:39:37 +0200

    feat: responsive layout

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ViewChild } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSidenavModule, MatSidenav } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
@@ -23,5 +23,32 @@ import { MatTooltipModule } from '@angular/material/tooltip';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  @ViewChild('drawer') drawer!: MatSidenav;
+  isSidenavOpen = true;
 
+  constructor() {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkScreenSize();
+  }
+
+  private checkScreenSize() {
+    if (typeof window !== 'undefined') {
+      this.isSidenavOpen = window.innerWidth >= 767.98;
+    } else {
+      this.isSidenavOpen = true;
+    }
+  }
+
+  /**
+   * Closes the navbar on mobile when navigation links are clicked
+   */
+  onNavLinkClick() {
+    if (typeof window !== 'undefined' && window.innerWidth < 767.98 && this.drawer) {
+      this.drawer.toggle();
+    }
+  }
 }
